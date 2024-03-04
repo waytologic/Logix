84e7390f7b4bb6dfb3f6b5441cc6a40c2d934ea7
Way2logic <128026796+waytologic@users.noreply.github.com>
2024-03-04 23:10:41 +0530

    update Layout

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent },
+  { path: '', component: LayoutComponent,
+    children:[
+      { path:'dashboard',component:DashboardComponent,title:'Dashboard'},
+    ]
+  },
 
 ];
 
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,13 +6,68 @@ import { Router } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
-
+export class LayoutComponent implements OnInit {
+  menuBtn:any;
+  header:any;
+  btns:any;
+  videos:any;
   constructor( private router:Router){
 
   }
 
+  ngOnInit(): void {
+    this.loadslider();
+  }
+
   login(){
     this.router.navigate(['./login']);
+  }
+
+  loadslider(){
+    let menuBtn = document.querySelector(".menu-btn");
+    let header = document.querySelector(".header");
+    let btns = document.querySelectorAll(".nav-item");
+    let videos = document.querySelectorAll("video");
+
+
+
+    btns.forEach((btn, i) => {
+      setTimeout(function(){
+
+
+
+      btn.addEventListener("click", () => {
+        btns.forEach((item) => {
+          item.classList.remove("active");
+        });
+
+        videos.forEach((video) => {
+          video.classList.remove("active");
+        });
+
+        videos[i].classList.add("active");
+        btn.classList.add("active");
+      });
+    }, 1000);
+    });
+
+  }
+
+  showlist(){
+    let header = document.querySelector(".header");
+    header?.classList.toggle("active");
+  }
+
+  slider(){
+
+  }
+  options(value:any){
+    if(value == 'fb'){
+      alert('fb')
+    }else if(value == 'instagram'){
+      alert('insta')
+    }else if(value == 'twitter'){
+      alert(' twitter X')
+    }
   }
 }
