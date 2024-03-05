5a9c58b644a89ea52b45f25225037f70afbc95f3
Way2logic <128026796+waytologic@users.noreply.github.com>
2024-03-05 23:06:03 +0530

    Add files via upload

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -4,7 +4,9 @@ import { UsersList,Usercred } from '../models/user';
 import { Observable, of } from 'rxjs';
 import { delay } from "rxjs/operators";
 
-const baseUrl = "http://localhost:3000";
+const APIUrl = "http://localhost:3000";
+const baseUrl = "https://my-json-server.typicode.com/waytologic/arpino";
+
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,15 +20,21 @@ export class UsersService {
   constructor(private http:HttpClient) { }
 
   getAll(): Observable<UsersList[]> {
-    return this.http.get<UsersList[]>(baseUrl+'/users',httpOptions);
+    return this.http.get<UsersList[]>(baseUrl+'/userlist',httpOptions);
   }
 
   Createuser(user:any) {
-    return this.http.post(baseUrl+'/users',user,httpOptions);
+    return this.http.post(APIUrl+'/users',user,httpOptions);
   }
 
   logout(): Observable<any> {
     return this.http.post(baseUrl + 'signout', { }, httpOptions);
+  }
+
+  isLoggedIn(): boolean {
+    // Implement your logic to check if the user is logged in
+    // For example, you can check if there is a token in local storage
+    return !!sessionStorage.getItem('loggedUser');
   }
 
 }
