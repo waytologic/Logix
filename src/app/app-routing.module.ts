adc1258ac67ee11826370071216cdb0fad379236
Way2logic <128026796+waytologic@users.noreply.github.com>
2024-03-05 23:04:45 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { title } from 'process';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path:'', loadChildren:()=> import('./layout/layout.module').then(m => m.LayoutModule), title:'Arpino', pathMatch:'full'},
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule),canActivate: [AuthGuard]  },
   { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
   { path: "**", loadChildren: () => import('./pagenotfound/pagenotfound.module').then(m => m.PagenotfoundModule) },
 ];
